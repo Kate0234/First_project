47e0e697be223ae645e8688367bc7d4eb85f67af
Kate <katewerner14091990@gmail.com>
2023-09-17 22:38:40 +0300

    initial input added

--- HW2.py
+++ HW2.py
@@ -1,1 +1,13 @@
-number =int(input("Enter any three numbers on your keyboard."))
+print("Let's calculate the maximum value of three numbers. Enter numbers and you will see the number with the highest value on the screen.")
+a, b, c = int(input("Enter the first number:")), int(input("Enter the second number:")), int(input("Enter the third number:")),
+mx = a
+if b > a > c:
+    mx = b
+elif c > a > b:
+    mx = c
+
+print(mx)
+
+print("Let's define the number with the minimum value.")
+a, b, c = int(input("Enter the first number:")), int(input("Enter the second number:")), int(input("Enter the third number:")),
+mn = a
