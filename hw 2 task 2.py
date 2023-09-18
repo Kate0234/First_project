024e943ec53dac41d660775cdcc864b2a2a27c0b
Kate <katewerner14091990@gmail.com>
2023-09-18 22:31:02 +0300

    last task added

---
+++ hw 2 task 2.py
@@ -0,0 +1,10 @@
+# Task 1-4
+meter = int(input("Enter the number of meters to be be converted into inches and yards:"))
+n = int(input("Press the number to make a choice: 1. Convert into inches, 2. Convert into yards. "))
+if n == 1:
+    print ( meter * 36.37 )
+elif n == 2:
+    print ( meter * 1.09 )
+else:
+    print("Wrong choice!")
+
