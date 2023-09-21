101240c7c4554e325dc4db0d275c73f6ed4402c2
Kate <katewerner14091990@gmail.com>
2023-09-21 22:57:47 +0300

    task added

--- HW2.py
+++ HW2.py
@@ -21,3 +21,12 @@ print("Enter 3 digit below to define the arithmetic value.")
 a, b, c = int(input("Enter the first number:")), int(input("Enter the second number:")), int(input("Enter the third number:")),
 result = ( a + b + c) / 3
 print(f"Result: {result}")
+
+meter = int(input("Enter the number of meters to be converted into inches and yards:"))
+n = int(input("Press the number to make a choice: 1. Convert into inches, 2. Convert into yards. "))
+if n == 1:
+    print ( meter * 36.37 )
+elif n == 2:
+    print ( meter * 1.09 )
+else:
+    print("Wrong choice!")
