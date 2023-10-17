6a7bdd86749febfe556614fd4a5d4c0cab825505
Kate <katewerner14091990@gmail.com>
2023-10-18 00:15:18 +0300

    1st commmit

---
+++ Additional tasks.py
@@ -0,0 +1,28 @@
+# Lesson 6.
+
+# Task 2.
+
+#Дано два списки чисел.Порахуйте, скільки чисел міститься як у першому списку, і у другому.
+
+#import random
+
+#numbers_1 = [random.randint(1, 10) for _ in range(10)]
+#print(numbers_1)
+
+#amount_of_numbers_list_1 = len(numbers_1)
+#print(amount_of_numbers_list_1)
+
+#numbers_2 = [random.randint(1, 50) for _ in range(23)]
+#print(numbers_2)
+
+#amount_of_numbers_list_2 = len(numbers_2)
+#print(amount_of_numbers_list_2)
+
+# Task 3.
+
+# Даний текст: у першому рядку записано число рядків, далі йдуть самі рядки.
+# Визначте, скільки різних слів міститься у цьому тексті.
+# Словом вважається послідовність непробільних символів, що йдуть поспіль,
+# слова розділені одним або більшим числом пробілів або символами кінця рядка.
+
+
